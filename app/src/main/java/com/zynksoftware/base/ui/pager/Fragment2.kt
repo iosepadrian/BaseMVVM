f39190a00c0f089d8c1fc5909530c6ddbcb38a2d
AdrianIosep <adrian.iosep@zynksoftware.com>
2022-05-03 11:02:26 +0300

    BaseDialogFragment

--- app/src/main/java/com/zynksoftware/base/ui/pager/Fragment2.kt
+++ app/src/main/java/com/zynksoftware/base/ui/pager/Fragment2.kt
@@ -1,10 +1,13 @@
 package com.zynksoftware.base.ui.pager
 
 import android.os.Bundle
+import com.zynksoftware.base.R
 import com.zynksoftware.base.databinding.FragmentDemoBinding
+import com.zynksoftware.base.ui.DemoDialogFragment
 import com.zynksoftware.base.ui.common.BaseFragment
 
-class Fragment2: BaseFragment<FragmentDemoBinding>(FragmentDemoBinding::inflate) {
+
+class Fragment2 : BaseFragment<FragmentDemoBinding>(FragmentDemoBinding::inflate) {
 
     companion object {
         fun newInstance(): Fragment2 {
@@ -13,6 +16,10 @@ class Fragment2: BaseFragment<FragmentDemoBinding>(FragmentDemoBinding::inflate)
     }
 
     override fun FragmentDemoBinding.onViewCreated(savedInstanceState: Bundle?) {
-        fragmentTitleTextView.text = "FRAGMENT 2"
+        fragmentTitleTextView.text = getString(R.string.fragmet_2)
+        dialogFragmentButton.setOnClickListener {
+            val dialog = DemoDialogFragment.newInstance()
+            dialog.show(requireActivity().supportFragmentManager, "Demo Dialog")
+        }
     }
 }
