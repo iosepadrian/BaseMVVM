ee5ad79b7c180c453c798775b820da30ba691741
iulianbuzila <iulian.buzila@zynksoftware.com>
2023-10-10 11:06:17 +0300

    update dependencies update compileSdk to 34 add versionCode mandatory to firebase config instead of flexible/immediate

--- app/src/main/java/com/zynksoftware/base/utils/FirebaseRemoteConfigUtils.kt
+++ app/src/main/java/com/zynksoftware/base/utils/FirebaseRemoteConfigUtils.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.zynksoftware.base.BuildConfig
 import com.zynksoftware.base.R
 
 class FirebaseRemoteConfigUtils(private val activity: Activity) {
@@ -13,7 +14,7 @@ class FirebaseRemoteConfigUtils(private val activity: Activity) {
         private val TAG = FirebaseRemoteConfigUtils::class.simpleName
         private const val MINIMUM_FETCH_INTERVAL_IN_SECONDS: Long = 120
 
-        private const val IN_APP_UPDATE_KEY = "in_app_update_type"
+        private const val BUILD_NUMBER_KEY = "buildNumberAndroid"
     }
 
     private val firebaseRemoteConfig = Firebase.remoteConfig
@@ -33,11 +34,15 @@ class FirebaseRemoteConfigUtils(private val activity: Activity) {
                     val updated = task.result
                     Log.d(TAG, "Config params updated: $updated")
 
-                    val inAppUpdateType = firebaseRemoteConfig.getString(IN_APP_UPDATE_KEY)
-                    if (inAppUpdateType == InAppUpdateType.FLEXIBLE.value) {
-                        onComplete.invoke(InAppUpdateType.FLEXIBLE)
-                    } else if (inAppUpdateType == InAppUpdateType.IMMEDIATE.value) {
+                    val buildNumberMandatory = firebaseRemoteConfig.getLong(BUILD_NUMBER_KEY).toInt()
+
+                    Log.d(TAG, "Remote config version code is $buildNumberMandatory")
+                    // If current version is lower than firebase version perform immediate update if is available.
+                    // otherwise check for flexible update if there is some new updates available
+                    if (BuildConfig.VERSION_CODE < buildNumberMandatory) {
                         onComplete.invoke(InAppUpdateType.IMMEDIATE)
+                    } else if (BuildConfig.VERSION_CODE > buildNumberMandatory){
+                        onComplete.invoke(InAppUpdateType.FLEXIBLE)
                     }
                 } else {
                     Log.d(TAG, "Config params update failed")
